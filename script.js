ad086a9fdcf9495ab46312017adafcb4cd55f1b1
Stephanie <steph.ferrandis.pro@outlook.fr>
2026-05-27 18:27:36 +0200

    CV

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ burger.addEventListener("click", () => {
     burger.classList.toggle("open");
     nav.classList.toggle("open");
 });
+
+// Icon + open/close de card curriculum
+
+const cards = document.querySelectorAll('.card-cv');
+
+cards.forEach(card => {
+    const opener = card.querySelector('.opener');
+    const icon = card.querySelector('.toggle-icon');
+
+    opener.addEventListener('click', () => {
+        card.classList.toggle('open');
+
+        if (card.classList.contains('open')) {
+            icon.src = "icons/minus.svg";
+            icon.alt = "Icon less";
+        } else {
+            icon.src = "icons/plus.svg";
+            icon.alt = "Icon more";
+        }
+    });
+});
